c18d38e40b156e6018e8a1b13a60e2e5d8c0c193
hendri54 <hendricksl@protonmail.com>
2021-04-27 10:23:08 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/UtilityFunctions.jl
@@ -0,0 +1,5 @@
+module UtilityFunctions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using UtilityFunctions
+using Test
+
+@testset "UtilityFunctions.jl" begin
+    # Write your tests here.
+end
